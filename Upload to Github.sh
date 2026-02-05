0d9261dd7a462e24bd889843ae174bd0eca37875
Ками Мир <umm.salya@MacBook-Pro-Kami.local>
2026-02-05 10:23:28 +0300

    Initial commit

---
+++ Upload to Github.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ссылка на GitHub репозиторий
+REPO_URL="git@github.com:ummsalya-alt/test-7.git"
+
+git remote remove origin
+git init
+git remote add origin $REPO_URL
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main
